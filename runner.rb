48275dc81e113458fca015c076a1c136e8c345cb
William Meighan <williamdmeighan@gmail.com>
2019-11-21 13:58:59 -0700

    Add runner file with initial questions

---
+++ runner.rb
@@ -0,0 +1,13 @@
+require './lib/games'
+require './lib/event'
+
+puts "Welcome to the Super Sports Games!"
+puts "Please enter the year for the current games:"
+print "> "
+
+year = gets.chomp
+
+puts "Please enter the events of this year's games: "
+print "> "
+
+events = gets.chomp
