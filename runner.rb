f7772be969a8bf4b3c94a0529b6e7cb5e6f88825
William Meighan <williamdmeighan@gmail.com>
2019-11-21 14:44:39 -0700

    Adding additional input - not yet working

--- runner.rb
+++ runner.rb
@@ -1,13 +1,46 @@
 require './lib/games'
 require './lib/event'
 
-puts "Welcome to the Super Sports Games!"
-puts "Please enter the year for the current games:"
-print "> "
+def start
+  puts "Welcome to the Super Sports Games!"
+  puts "Please enter the year for the current games:"
+  print "> "
+  year = gets.chomp
 
-year = gets.chomp
+  puts "Please enter the first event of this year's games:"
+  print "> "
+  event = gets.chomp
 
-puts "Please enter the events of this year's games: "
-print "> "
+  additional_games
 
-events = gets.chomp
+  puts "Please enter the ages of this year's competitors:"
+  print "> "
+  ages = gets.chomp
+end
+
+def additional_games
+  puts "Would you like to add any additional games? Enter Y or N:"
+  print "> "
+  answer = gets.chomp
+
+  until answer.upcase == "Y" || answer.upcase == "N"
+    puts "Please enter valid input: "
+    print "> "
+    answer = gets.chomp
+  end
+
+  if answer.upcase == "Y"
+    puts "Please enter the next event: "
+    print "> "
+    new_event = gets.chomp
+  end
+
+  until answer.upcase == "N"
+    answer = gets.chomp
+  end
+end
+
+start
+
+events = Event.new(event, ages)
+games = Games.new(year)
